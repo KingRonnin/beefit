41d0aa409cc0d2e37d5564731d6f69539bb3f5c1
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-03 04:46:31 -0600

    deleting file

--- front_end/src/api/records.tsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const getRecord = () => {
-    return axios.get('/api/records');
-};
-
-export const addRecord = () => {
-    return axios.post('/api/records', record);
-};
