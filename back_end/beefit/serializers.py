e916e2cbfed7d62c9d97a455e298fd11779314de
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-08 00:54:16 -0600

    more database stuff

--- back_end/beefit/serializers.py
+++ back_end/beefit/serializers.py
@@ -1,3 +1,12 @@
 from rest_framework import serializers
-from .models import Todo
+from .models import Exercise, DailyLog
 
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ('id', 'name')
+        
+class DailyLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyLog
+        fields = ('id', 'date', 'calories_intake', 'calories_burnt', 'steps')
--- back_end/beefit/views.py
+++ back_end/beefit/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ExerciseSerializer, DailyLogSerializer
+from .models import Exercise, DailyLog
 
 # Create your views here.
+
+class ExerciseView(viewsets.ModelViewSet):
+    serializer_class = ExerciseSerializer
+    queryset = Exercise.objects.all()
+
+class DailyLogView(viewsets.ModelViewSet):
+    serializer_class = DailyLogSerializer
+    queryset = DailyLog.objects.all()
