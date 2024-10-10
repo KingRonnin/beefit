6564ea3194e541e6304bcc9a3be8ccf4866bb634
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-10 02:26:49 -0600

    I want to test sign in & sign up page

--- src/routes/SignInPage.tsx
+++ src/routes/SignInPage.tsx
@@ -1,33 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignInPage.css';
 
 export default function SignInPage() {
+
+  const [action,setAction] = useState("Sign Up");
+
     return (
       <>
         <div className="left-section" />
         <div className="right-section">
           <div className="header">
-            <div className="text">Sign Up</div>
+            <div className="text">[action]</div>
             <div className="underline"></div>
           </div>
           <div className="input-group">
+            {action==="Login"?<div></div>:
+            <div className="input">
+              <input type="text" placeholder='Name' />
+            </div>}
 
             <div className="input">
-              <input type="text" />
+              <input type="email" placeholder='Email' />
             </div>
 
             <div className="input">
-              <input type="email" />
-            </div>
-
-            <div className="input">
-              <input type="password" />
+              <input type="password" placeholder='Password' />
             </div>
           </div>
-          <div className="forgot-password">Lost Password? <span>Click here</span></div>
+          {action==="Sign Up"?<div></div>:
+          <div className="forgot-password">Lost Password? <span>Click here</span></div>}
           <div className="submit-container">
-            <div className="submit"> Sign Up </div>
-            <div className="submit">Login</div>
+            <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
           </div>
         </div>
       </>
