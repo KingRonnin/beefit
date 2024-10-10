2c04871651bf6c41535fcafe9a221f9baf921f29
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-10 02:09:07 -0600

    css update and sign in update

--- src/routes/SignInPage.tsx
+++ src/routes/SignInPage.tsx
@@ -1,21 +1,35 @@
-// import { SignIn } from '@clerk/clerk-react'
-
-// export default function SignInPage() {
-//   return <SignIn path="/sign-in" />
-// }
+import React from 'react'
+import './SignInPage.css';
 
 export default function SignInPage() {
-  return (
-    <>
-      <body>
-        <div className="sign-in">
-          
-          <div className="left-section" />
-          <div className="right-section">
+    return (
+      <>
+        <div className="left-section" />
+        <div className="right-section">
+          <div className="header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+          </div>
+          <div className="input-group">
 
+            <div className="input">
+              <input type="text" />
+            </div>
+
+            <div className="input">
+              <input type="email" />
+            </div>
+
+            <div className="input">
+              <input type="password" />
+            </div>
+          </div>
+          <div className="forgot-password">Lost Password? <span>Click here</span></div>
+          <div className="submit-container">
+            <div className="submit"> Sign Up </div>
+            <div className="submit">Login</div>
           </div>
         </div>
-      </body>
-    </>
-  )
+      </>
+    )
 }
