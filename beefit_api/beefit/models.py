5bc97b45773b0dfade84b5c25efbf837ae5d228a
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-15 21:14:28 -0600

    creating a register-test-page

--- beefit_api/beefit/models.py
+++ beefit_api/beefit/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=25)
+    username = models.CharField(max_length=20)
     email = models.EmailField(max_length=254)
-    password = models.CharField(max_length=25)
+    password = models.CharField(max_length=100)
     created_date = models.DateTimeField(auto_now_add=True)
     
-
+    def __str__(self):
+        return super().__str__()
