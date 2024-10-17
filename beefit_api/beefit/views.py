2f265c45dabc37e7c46e4c4a0fe9ed5b321e2f35
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-17 00:18:09 -0600

    implimenting login function from django

--- beefit_api/beefit/views.py
+++ beefit_api/beefit/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("beefit:login")
     else:   
         form = UserCreationForm()
@@ -17,6 +18,7 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
+            login(request, form.get_user())
             return redirect('/')
     else:
         form = AuthenticationForm()
