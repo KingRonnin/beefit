359838168fc4060ff971aa93c7c80f8f042e140f
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-16 23:58:10 -0600

    registration page

--- beefit_api/beefit/views.py
+++ beefit_api/beefit/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def register_view(request):
-    if request.POST == 'POST':  
-        form = UserCreationForm()  
-        if form.is_valid():  
-            form.save()  
-    messages.success(request, 'Account created successfully')  
-    else:  
-        form = UserCreationForm() 
-    
-    context = {  
-        'form':form  
-    }
-    return render(request, 'users/register.html', context)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:   
+        form = UserCreationForm()
+        
+    return render(request, 'users/register.html', { 'form': form })
