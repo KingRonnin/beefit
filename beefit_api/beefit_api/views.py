01eed7779d9811dd17d19a7b43270dde0e725410
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-17 00:57:12 -0600

    fixed user authorization

--- beefit_api/beefit_api/views.py
+++ beefit_api/beefit_api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='beefit/login')
 def homepage (request):
     return render(request, 'homepage.html')
