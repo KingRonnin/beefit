4c1da6701e8b63701ae8328e11501a88e95e9b67
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-23 17:22:05 -0600

    registering models to admin dashboard page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,3 +4,6 @@ from api import models as api_models
 
 # Register your models here.
 admin.site.register(api_models.User)
+admin.site.register(api_models.Exercise)
+admin.site.register(api_models.Strength)
+admin.site.register(api_models.Cardiovascular)
