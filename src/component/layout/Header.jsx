782a4e6ab45bf12c52f6820a0b69a35edb5c19d4
kaurrajinder0 <141068515+kaurrajinder0@users.noreply.github.com>
2024-10-23 14:59:31 -0600

    Header done

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -1,26 +1,25 @@
-// Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';  // Ensure you're importing the correct CSS file
+import './Header.css';  // Import the CSS file for styling
 
 const Header = () => {
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/" className="nav-link">Home</Link>
-        </li>
-        <li>
-          <Link to="/about" className="nav-link">About</Link>
-        </li>
-        <li>
-          <Link to="/services" className="nav-link">Services</Link>
-        </li>
-        <li>
-          <Link to="/contact" className="nav-link">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="header">
+      <nav className="navbar">
+        <div className="navbar-left">
+          <Link to="/" className="brand-name">Beefit</Link>  {/* Beefit on the left */}
+        </div>
+        <ul className="nav-menu">
+          <li><Link to="/" className="nav-link">Home</Link></li>
+          <li><Link to="/about" className="nav-link">About</Link></li>
+          <li><Link to="/services" className="nav-link">Services</Link></li>
+          <li><Link to="/contact" className="nav-link">Contact</Link></li>
+        </ul>
+        <div className="navbar-right">
+          <Link to="/login" className="nav-link login-link">Login</Link>  {/* Login on the right */}
+        </div>
+      </nav>
+    </header>
   );
 };
 
