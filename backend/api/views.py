4a29abb857c7acf318e25a38a181a9ab1799eca4
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-24 00:27:06 -0600

    views.py imports save

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.db.models import Sum
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes, APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+
+import json
+import random
+
+from api import serializers as api_serializers
+from api import models as api_models
 
 # Create your views here.
+
