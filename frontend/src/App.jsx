0051ad6d4b97f48cf3343528290e12a28627e2b6
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-26 02:35:36 -0600

    removed unnecessary lines of code inside the App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import MyPlan from './views/routes/MyPlan.tsx';
 import LogFitness from './views/routes/LogFitness.tsx';
 import Calender from './views/routes/Calendar.jsx';
 import SignIn from './views/routes/SignIn.jsx';
-import Header from './views/component/Header.jsx';
 
 
 
@@ -18,10 +17,8 @@ function App() {
           <Route path="/" element={<PlanFrontPage />} />
           <Route path="/myplan" element={<MyPlan />} />
           <Route path="/logfitness" element={<LogFitness />} />
-          <Route path="/PlanFrontPage" element={<PlanFrontPage />} />
           <Route path="/Calender" element={<Calender />} />
           <Route path="/SignIn" element={<SignIn />} />
-          <Route path="/Header" element={<Header />} />
         </Routes>
       </div>
     </Router>
