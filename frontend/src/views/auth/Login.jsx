e8082ced31e1588587f57210181ae1b41025596f
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-28 00:32:01 -0600

    login functionality no return

--- frontend/src/views/auth/Login.jsx
+++ frontend/src/views/auth/Login.jsx
@@ -5,5 +5,35 @@ import { useAuthStore } from '../../store/auth.js';
 import { register } from '../../utils/auth.js';
 
 function Login() {
-    
+    const [bioData, setBioData] = useState({ email: "", password: "" });
+    const [isLoading, setIsLoading] = useState(false);
+    const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+    const navigate = useNavigate();
+
+    const handleBioDataChange = (event) => {
+        setBioData({
+            ...bioData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const resetForm = () => {
+        setBioData({
+            email: "",
+            password: "",
+        });
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        setIsLoading(true);
+
+        const { error } = await login(bioData.email, bioData.password);
+        if (error) {
+            alert(JSON.stringify(error));
+        } else {
+            navigate('/');
+        }
+        setIsLoading(false);
+    };
 }
