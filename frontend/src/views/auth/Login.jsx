03e7f65a9a7df3ee713a4153537583028ab5e298
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-10-28 01:22:47 -0600

    login page

--- frontend/src/views/auth/Login.jsx
+++ frontend/src/views/auth/Login.jsx
@@ -36,4 +36,54 @@ function Login() {
         }
         setIsLoading(false);
     };
+
+    return (
+        <>
+            <section className="container">
+                <div className="box">
+                    <h2>Login</h2>
+                    <form className='needs-valdiation' onSubmit={handleLogin} noValidate>
+                        {/* username */}
+                        <label htmlFor="email" className='form-label'>
+                            Email Address
+                        </label>
+                        <input type="email" onChange={handleBioDataChange} value={bioData.email} id='email' className='form-control' name='email' placeholder='johndoe@gmail.com' required />
+                        <div className='invalid-feedback'>Please enter valid username</div>
+                        {/* password */}
+                        <label htmlFor="password" className='form-label'>
+                            Password
+                        </label>
+                        <input type="password" onChange={handleBioDataChange} value={bioData.password} id='password' classname='form-control' name='password' placeholder='********' required />
+                        <div className='invalid-feedback'>Please enter a valid password</div>
+                        {/* checkbox */}
+                        <div className='form-check'>
+                            <input type="checkbox" className='form-check-iput' id='rememberme' required />
+                            <label htmlFor="rememberme">
+                                Remember Me
+                            </label>
+                            <div className='invalid-feedback'>You must agree before submitting</div>
+                        </div>
+                        <div>
+                            {/* <Link to=></Link> */}
+                        </div>
+                        <button type='submit' disabled={isLoading}>
+                            {isLoading} ? (
+                                <>
+                                    <span>Processing...</span>
+                                    <i className='fas fa-spinner fa-spin'/>
+                                </>
+                            ) : (
+                                <>
+                                    <span>
+                                        Sign In
+                                    </span>
+                                    <i className='fas fa-sign-in-alt' />
+                                </>
+                            )
+                        </button>
+                    </form>
+                </div>
+            </section>
+        </>
+    )
 }
