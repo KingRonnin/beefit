8295cdeed6030965c7fffaae485464d0b80f8cff
kaurrajinder0 <141068515+kaurrajinder0@users.noreply.github.com>
2024-10-30 16:24:45 -0600

    workoutlogPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import SignIn from './views/routes/SignIn.jsx';
 import Register from './views/auth/Register.jsx';
 import Login from './views/auth/Login.jsx';
 import About from './views/routes/About.jsx';
-
+import WorkoutLogPage from './views/routes/WorkoutLog.jsx';
 
 function App() {
   return (
@@ -24,6 +24,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/About" element={<About />} />
+                <Route path="/WorkoutLog" element={<WorkoutLogPage />} />
 
 
               </Routes>
