f9b27914bb8a9a2ae6aeb5c78ca349b29034408f
KingRonnin <ronaldo.chuaiii@edu.sait.ca>
2024-11-03 23:19:37 -0700

    url set

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('get/exercise/strength/', api_views.StrengthListAPIView.as_view()),
     path('get/exercise/cardio/', api_views.CardiovascularListAPIView.as_view()),
     
-    path('post/strength/log/', api_views.LogStrengthView.as_view()),
+    path('post/log/strength/', api_views.LogStrengthView.as_view()),
+    path('post/log/cardiovascular/', api_views.LogCardioView.as_view()),
     
     path('dashboard/exercise/<user_id>/', api_views.UserExerciseView.as_view()),
     path('dashboard/strength/<user_id>/', api_views.UserStrengthExerciseView.as_view()),
