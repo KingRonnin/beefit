9b4df57dd6adf491de01dd1aede9bca2b6925ce0
kaurrajinder0 <141068515+kaurrajinder0@users.noreply.github.com>
2024-11-18 12:36:00 -0700

    c

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from api import views as api_views
 
 urlpatterns = [
@@ -20,4 +23,7 @@ urlpatterns = [
     
     path('create-payment-intent/', api_views.create_payment_intent, name='create_payment_intent'),
     path('create-checkout-session/', api_views.create_checkout_session, name='create-checkout-session'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
